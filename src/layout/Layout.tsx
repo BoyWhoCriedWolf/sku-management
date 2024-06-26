f5aefa5abb0efcad1f879656253b150e3367c8d8
BoyWhoCriedWolf <hdreamdev@gmail.com>
2024-06-27 00:45:16 +0800

    fix: container

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,7 +1,8 @@
+import { Container } from "@mui/material";
 import React, { FC, PropsWithChildren } from "react";
 
 const Layout: FC<PropsWithChildren> = ({ children }) => {
-  return <div>{children}</div>;
+  return <Container sx={{ py: 4 }}>{children}</Container>;
 };
 
 export default Layout;
